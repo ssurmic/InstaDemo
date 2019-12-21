83b465528c37275bf6548388ab3f26a37ea4f78d
zizhao zhang <ssurmiczizhao@gmail.com>
2019-12-21 18:49:29 -0500

    chaneg db format to deploy on heroku

--- InstaDemo/settings.py
+++ InstaDemo/settings.py
@@ -77,12 +77,13 @@ WSGI_APPLICATION = 'InstaDemo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 LOGIN_REDIRECT_URL = 'home'
 
